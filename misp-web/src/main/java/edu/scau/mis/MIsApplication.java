3038d9a947410a59aa623ae7e9022a41df18631b
moom-debug <72612685+moom-debug@users.noreply.github.com>
2021-09-28 20:26:52 +0800

    try

--- misp-web/src/main/java/edu/scau/mis/MIsApplication.java
+++ misp-web/src/main/java/edu/scau/mis/MIsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MIsApplication {
     public static void main(String[] args) {
         SpringApplication.run(MIsApplication.class, args);
-        System.out.println("MIS开发平台启动成功!");
+        System.out.println("MIS开发平台启动成功!!!");
     }
 
 }
